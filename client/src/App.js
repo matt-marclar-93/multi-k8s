e472064857e5315d7c736be5b3ab9a9b10de28a8
Matthew Marbury <mmarbury@openlending.com>
2021-07-13 08:54:48 -0500

    Trying to get my images and code working instead of Stephan's

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React 3
+            This is cincobot's code!
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
